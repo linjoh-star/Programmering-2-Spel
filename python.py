fee4608e452294184f89e7aa0e0a445c16e015db
linjoh-star <linusjohansson0505@gmail.com>
2023-03-30 22:33:49 +0200

    added bullet and collectible

--- python.py
+++ python.py
@@ -8,7 +8,7 @@ pygame.init()
 cwd = os.getcwd()
 
 # Set up the display
-screen_width = 800 
+screen_width = 800
 screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("2D-Game")
@@ -20,20 +20,38 @@ FONT = pygame.font.SysFont("Arial", 30)
 file_path = os.path.join("background.png")
 file_path1 = os.path.join("blaha.png")
 file_path2 = os.path.join("pixil-frame-0.png")
+file_path3 = os.path.join("collectible.png")
+file_path4 = os.path.join("bullet.png")
 BACKGROUND_IMAGE = pygame.image.load(file_path).convert()
 PLAYER_RIGHT = pygame.image.load(file_path2).convert_alpha()
 PLAYER_LEFT = pygame.image.load(file_path1).convert_alpha()
+NOT_RACIST_COLLECTABLE = pygame.image.load(file_path3).convert_alpha()
+BULLET = pygame.image.load(file_path4).convert_alpha()
 
-PLAYER_RIGHT = pygame.transform.scale(PLAYER_RIGHT, (100,100))
-PLAYER_LEFT = pygame.transform.scale(PLAYER_LEFT, (100,100))
+PLAYER_RIGHT = pygame.transform.scale(PLAYER_RIGHT, (100, 100))
+PLAYER_LEFT = pygame.transform.scale(PLAYER_LEFT, (100, 100))
+NOT_RACIST_COLLECTABLE = pygame.transform.scale(PLAYER_RIGHT, (100, 100))
+BULLET = pygame.transform.scale(PLAYER_LEFT, (10, 10))
+
+
+class Collectible:
+    def __init__(self, x, y, width=30, height=30, image_path="collectible.png"):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (width, height))
+
+    def spawn(self):
+        self.rect.x = random.randint(0, screen_width - self.rect.width)
+        self.rect.y = random.randint(0, screen_height - self.rect.height)
 
 
 class Score:
-     def __init__(self, text, x, y, width=200, height=50, color=(255, 255, 255), text_color=(0, 0, 0)):
+    def __init__(self, text, x, y, width=200, height=50, color=(255, 255, 255), text_color=(0, 0, 0)):
         self.text = text
-        self.x = x 
+        self.x = x
         self.y = y
         text_surf = FONT.render(text, True, text_color)
+
 
 class Button:
     def __init__(self, text, x, y, width=200, height=50, color=(255, 255, 255), text_color=(0, 0, 0)):
@@ -48,14 +66,13 @@ class Button:
         surface.blit(self.surface, self.rect)
 
 
-
-
 class Player:
     def __init__(self, x, y, width=40, height=40, speed=4, lives=3):
         self.rect = pygame.Rect(x, y, width, height)
         self.image = PLAYER_LEFT
         self.speed = speed
         self.lives = lives
+
 
     def move(self, keys, screen_width, screen_height):
         if keys[pygame.K_w] and self.rect.top > 0:
@@ -68,8 +85,7 @@ class Player:
         if keys[pygame.K_d] and self.rect.right < screen_width:
             self.rect.right += self.speed
             self.image = PLAYER_RIGHT
-        pygame.display.flip()        
-        
+        pygame.display.flip()
 
 
 class Enemy:
@@ -86,12 +102,17 @@ class Enemy:
         if self.rect.top > screen_height:
             self.rect.top = 0
             self.rect.left = random.randint(0, screen_width - self.rect.width)
-        print(self.xvel, self.yvel)
         self.rect.left += self.speed * self.xvel
         if self.rect.left > screen_width:
             self.rect.left = 0
             self.rect.top = random.randint(0, screen_height - self.rect.height)
 
+class Bullet:
+    def __init__(self, x, y, target_x, target_y):
+        self.rect = pygame.Rect(x, y, 10, 10)
+        self.target_x = target_x
+        self.target_y = target_y
+        self.image = BULLET 
 
 class Game:
     def __init__(self):
@@ -99,14 +120,19 @@ class Game:
         self.player = Player(screen_width // 2 - 25, screen_height - 60)
         self.enemies = [Enemy(random.randint(0, screen_width - 30), random.randint(0, screen_height // 2), xvel=1)
                         for _ in range(3)] + [Enemy(random.randint(0, screen_width - 30), random.randint(0, screen_height // 2), yvel=1)
-                        for _ in range(3)] + [Enemy(random.randint(0, screen_width - 30), random.randint(0, screen_height // 2), yvel=1, xvel=1)
-                        for _ in range(3)]
-        self.start_button = Button("Start", screen_width // 2 - 100, screen_height // 2)
-        self.end_button = Button("Restart", screen_width // 2 - 100, screen_height // 2)
+                                              for _ in range(3)] + [Enemy(random.randint(0, screen_width - 30), random.randint(0, screen_height // 2), yvel=1, xvel=1)
+                                                                    for _ in range(3)]
+        self.start_button = Button(
+            "Start", screen_width // 2 - 100, screen_height // 2)
+        self.end_button = Button(
+            "Restart", screen_width // 2 - 100, screen_height // 2)
         self.game_state = "start_menu"
         self.running = True
         self.show_end_screen = False
         self.frame = 0
+        self.collectibles = [Collectible(random.randint(0, screen_width - 30), random.randint(0, screen_height - 30))
+                             for _ in range(5)]
+        self.bullets = []
 
     def handle_events(self):
         for event in pygame.event.get():
@@ -118,17 +144,21 @@ class Game:
                     self.game_state = "playing"
                 elif self.game_state == "end_screen" and self.end_button.rect.collidepoint(mouse_pos):
                     self.reset_game()
+                elif self.game_state == "playing":
+                    print(mouse_pos)
+                    self.bullets.append(Bullet(self.player.rect.x, self.player.rect.y, mouse_pos[0], mouse_pos[1]))
 
         if self.game_state == "playing":
             keys = pygame.key.get_pressed()
             self.player.move(keys, screen_width, screen_height)
+        
 
     def update(self):
-
         if self.game_state == "playing":
             for enemy in self.enemies:
-                color = colorsys.hls_to_rgb((self.frame % 100) / 100 , 0.5, 1)
-                enemy.image.fill((color[0] * 255, color[1] * 255, color[2] * 255))
+                color = colorsys.hls_to_rgb((self.frame % 100) / 100, 0.5, 1)
+                enemy.image.fill(
+                    (color[0] * 255, color[1] * 255, color[2] * 255))
                 enemy.move(screen_width, screen_height)
                 if enemy.rect.colliderect(self.player.rect):
                     self.player.lives -= 1
@@ -136,6 +166,13 @@ class Game:
                         self.show_end_screen = True
             if not self.enemies:
                 self.show_end_screen = True
+            
+            for collectible in self.collectibles:
+                if self.player.rect.colliderect(collectible):
+                    collectible.spawn()
+
+            for bullet in self.bullets:
+
 
         self.frame += 1
 
@@ -148,9 +185,17 @@ class Game:
             screen.blit(self.player.image, self.player.rect)
             for enemy in self.enemies:
                 screen.blit(enemy.image, enemy.rect)
+
+            for collectible in self.collectibles:
+                screen.blit(collectible.image, collectible.rect)
+
+            for bullet in self.bullets:
+                screen.blit(bullet.image, bullet.rect)
+
         elif self.game_state == "end_screen":
             end_text = FONT.render("Game Over", True, (255, 0, 0))
-            end_text_rect = end_text.get_rect(center=(screen_width // 2, screen_height // 2 - 50))
+            end_text_rect = end_text.get_rect(
+                center=(screen_width // 2, screen_height // 2 - 50))
             screen.blit(end_text, end_text_rect)
             self.end_button.draw(screen)
 
@@ -158,8 +203,8 @@ class Game:
         self.player = Player(screen_width // 2 - 25, screen_height - 60)
         self.enemies = [Enemy(random.randint(0, screen_width - 30), random.randint(0, screen_height // 2), xvel=1)
                         for _ in range(3)] + [Enemy(random.randint(0, screen_width - 30), random.randint(0, screen_height // 2), yvel=1)
-                        for _ in range(3)] + [Enemy(random.randint(0, screen_width - 30), random.randint(0, screen_height // 2), yvel=1, xvel=1)
-                        for _ in range(3)]
+                                              for _ in range(3)] + [Enemy(random.randint(0, screen_width - 30), random.randint(0, screen_height // 2), yvel=1, xvel=1)
+                                                                    for _ in range(3)]
         self.show_end_screen = False
         self.game_state = "playing"
 
@@ -171,8 +216,10 @@ class Game:
             pygame.display.update()
             self.clock.tick(60)
 
+
             if self.show_end_screen:
                 self.game_state = "end_screen"
+
 
 if __name__ == "__main__":
     pygame.init()
